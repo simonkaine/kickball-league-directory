fd87fea0c896cc4de373974369962165ec2c0d59
simonkaine <simonkaine@gmail.com>
2021-12-08 10:02:44 -0800

    Team details test pass.

---
+++ src/Views/Teams/TeamDetails.test.jsx
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import TeamDetails from "./TeamDetails";
+
+it('Should test for detailed view of teams list', async () => {
+    const {container} = render(
+        <MemoryRouter>
+            <TeamDetails match={{ params: {teamId: '1'} }}/>
+        </MemoryRouter> 
+    );
+    
+    screen.getByText('...Loading team');
+    const theTeamName = await screen.findByText(/Bridge City Sneakers/i);
+    expect(theTeamName).toBeInTheDocument()
+    expect(container).toMatchSnapshot();
+});
